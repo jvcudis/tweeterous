1b1947708c6da84dc8136095b255fcf4b04507ae
Joann Cudis <jcudis@exist.com>
2011-11-22 08:50:20 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tweeterous::Application.config.secret_token = 'cb00cd0c95293711fee6cbcfdf3a40775d3cdeb9fdd0123acc59b91a9f4ff9c86b5f9b7a5a7d43fe9c350f88a06a6ad278e2e65789e9f68a1b16f5cab80802b1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Tweeterous::Application.config.session_store :cookie_store, key: '_tweeterous_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Tweeterous::Application.config.session_store :active_record_store
